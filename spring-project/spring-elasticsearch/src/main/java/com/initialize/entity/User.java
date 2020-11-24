b49401ca208cda4810e62b313ee86b0c096ec685
sertachak <sertaconurhakbilen@gmail.com>
2020-11-24 19:58:12 +0300

    Elastic Search Part1

---
+++ spring-project/spring-elasticsearch/src/main/java/com/initialize/entity/User.java
@@ -0,0 +1,30 @@
+package com.initialize.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.Date;
+
+@Document(indexName = "users" )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+    @Field(name= "name", type = FieldType.Text)
+    private String name;
+    @Field(name = "surname", type = FieldType.Text)
+    private String surname;
+    @Field(name = "Address", type = FieldType.Text)
+    private String address;
+    @Field(name="birthDay", type = FieldType.Text)
+    private Date birthDay;
+}
---
+++ spring-project/spring-elasticsearch/src/main/java/com/initialize/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.initialize.repository;
+
+import com.initialize.entity.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends ElasticsearchRepository<User, String> {
+}
